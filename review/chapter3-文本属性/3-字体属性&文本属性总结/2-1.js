79c4b53673173c963ab1bf427b4fac7182dacf86
wintimes <842110408@qq.com>
2020-04-24 23:50:44 +0800

    review -1

---
+++ review/chapter3-文本属性/3-字体属性&文本属性总结/2-1.js
@@ -0,0 +1,56 @@
+/*
+    字体属性，文本属性
+        字体属性
+            字体
+                font-family: "微软雅黑","宋体"
+            字体大小
+                font-size:20px;
+                像素单位：px, em, rem
+                px:绝对单位，一旦设置了值就不会再改变大小了
+                em:相对单位，相对模块区域的字体大小，
+                rem:主要应用于移动
+            字体颜色
+                color
+                    单词
+                    rgb
+                    rgba
+                    16进制
+            字体粗细
+                font-style:
+                    normal
+                    italic
+            字体样式（？）
+                font-weight:
+                    bold
+                    100-900
+                    400默认
+
+        文本属性
+            文本修饰
+                text-decoration:
+                    underline 下划线
+                    none 无
+                    overline 上划线
+                    line-through 删除线
+            文本缩进
+                text-indent
+                    单位建议使用em
+            行高
+                行于行之间的距离
+                line-height
+                    px, em, rem
+            中文/英文单词间距
+                中文
+                    letter-spacing
+                英文
+                    word-spacing
+            文本对齐方式
+                text-align
+                    center
+                    left
+                    right
+*/
+//综合属性
+// font: 20px / 3  "Helvetica Neue", Helvetica,Arial,Microsoft Yahei,Hiragino Sans GB,Heiti SC,
+//WenQuanYi Micro Hei,sans-serif;
+//sans-serif 如果所列字体都没有，还可以有显示的字体。
